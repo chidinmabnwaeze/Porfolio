fba751200b669dae5e4a57331c7636e6a566892d
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-16 11:35:47 +0100

    card

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const projects = () => {
   return (
-    <div className='projects'>
-        <div className="heading">
-            <h4>Featured Projects</h4>
-            <hr />
-        </div>
-        <div className="project-card">
-            <img src="" alt="" />
-            <div className="card-details">
-                
+    <div className="projects">
+      <div className="heading">
+        <h4>Featured Projects</h4>
+        <hr />
+      </div>
+      <div className="project-card">
+        <img src="" alt="" />
+        <div className="card-details">
+          <div className="cardName">
+            <div className="title">
+              <h3>SpeedKash</h3>
+              <p className="sub-text">Fintech Web App</p>
             </div>
+          </div>
+          <div className="cardText">
+            <p>
+              Enabling users save, take loan and shop for products and services
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default projects
+export default projects;
