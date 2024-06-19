4fd0149087d9de3425d3a171f5380b7e113a998c
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-19 16:04:35 +0100

    brands

--- src/components/brands.jsx
+++ src/components/brands.jsx
@@ -6,6 +6,9 @@ export default function Brands() {
     {
       img: org1,
     },
+    {
+        img: org1,
+      }
   ];
 
   return (
@@ -14,11 +17,13 @@ export default function Brands() {
         <p>Some Organizations I've worked with</p>
         <hr />
       </div>
-      {Org.map((org, index) => {
+      <div className="orgLogo">
+      {Org.map((org, index) => (
         <div className="orgs" key={index}>
-          <img className="org1" src={org.img} alt="" />
-        </div>;
-      })}
+          <img className="org" src={org.img} alt="" />
+        </div>
+      ))};
+      </div>
     </div>
   );
 }
